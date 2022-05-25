d9b24bf1dab92872cacc240707945b9f0926d28e
Ryan Kirkpatrick <Rk168810@gmail.com>
2022-05-24 21:40:00 -0700

    Setup Google Analytics

--- public/javascript/firebase-setup.js
+++ public/javascript/firebase-setup.js
@@ -1,28 +1,26 @@
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-var firebaseConfig = {
-	apiKey: "AIzaSyC6n_0MqCxrZIIWM_smT9eVjKRMeBpWMG8",
-	authDomain: "theory-parking.firebaseapp.com",
-	databaseURL: "https://theory-parking.firebaseio.com",
-	projectId: "theory-parking",
-	storageBucket: "theory-parking.appspot.com",
-	messagingSenderId: "192548003681",
-	appId: "1:192548003681:web:75dfba17644c49dd9caf20",
-	measurementId: "G-6S8CGR6TWP"
+const firebaseConfig = {
+	apiKey: "AIzaSyAxdGbBiX_L9uIu0KotlTbWzgH9roIVnls",
+	authDomain: "generic-sensor-display.firebaseapp.com",
+	projectId: "generic-sensor-display",
+	storageBucket: "generic-sensor-display.appspot.com",
+	messagingSenderId: "78111896061",
+	appId: "1:78111896061:web:806a6fba1b5de05060e994",
+	measurementId: "G-RJF2G33QR3",
 };
 window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
-if (window.location.hostname == "localhost")
-{
-	//TODO Can be removed for deployment
-	gtag('config', 'UA-148151012-3', {'debug_mode':true});
-	gtag('config', 'G-6S8CGR6TWP', {'debug_mode':true});
+function gtag() {
+	dataLayer.push(arguments);
 }
-else
-{
-	gtag('config', 'UA-148151012-3');
-	gtag('config', 'G-6S8CGR6TWP');
+gtag("js", new Date());
+if (window.location.hostname == "localhost") {
+	//TODO Can be removed for deployment
+	// gtag('config', 'UA-148151012-3', {'debug_mode':true});
+	gtag("config", "G-RJF2G33QR3", { debug_mode: true });
+} else {
+	// gtag('config', 'UA-148151012-3');
+	gtag("config", "G-RJF2G33QR3");
 }
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
